406903449c038066bc26df0ebfda0d4d24260537
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2025-12-20 13:45:28 +0100

    1766234726433447938

--- src/zsleep.js
+++ src/zsleep.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 /*
+ *
  * Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
- * https://kekse.biz/
+ * https://kekse.biz/  https://github.com/kekse1/zsleep/
+ *
  */
 
 //
