5eff5888924b3cb8a588f186350cc57f1c40dc97
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2026-01-06 12:45:56 +0100

    1767699955531500735

--- src/zsleep.js
+++ src/zsleep.js
@@ -872,7 +872,7 @@ const end = (_fin, _runtime, _millisec, _param) => {
 	{
 		const diff = Math.max(0, (_millisec - _runtime));
 		if(_param.print) console.error(
-			'\n(aborted by SIGINT)\n         End: ' + new Date().toGMTString() +
+			'\n(aborted by SIGINT)\n    Real End: ' + new Date().toGMTString() +
 			'\n     Runtime: ' + Math.time.render(_runtime) + '\n  Difference: ' +
 			Math.time.render(diff) + '\nMilliseconds: ' + diff.toLocaleString() +
 			'\n     Seconds: ' + Math.round(diff / 1000, PRECISION).toFixed(PRECISION));
